77775dce4c09518bf44f16b6940d4b54aceb9a5e
TotoShefulThau <37251093+TotoShefulThau@users.noreply.github.com>
2021-10-28 03:17:16 +0300

    checkout/wishlist
    - de terminat customer page

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 console.log("script loaded");
 
+//check shipping adress
+const checkdButton = document.getElementById("dif-address");
+const movable_div = document.getElementById("movable_div");
+
+checkdButton.onclick = () => {
+  let status = checkdButton.checked;
+  if (status) {
+    movable_div.style.height = `0%`;
+  } else {
+    movable_div.style.height = `100%`;
+  }
+};
